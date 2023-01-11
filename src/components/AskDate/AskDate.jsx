5e050dd3c6354d8bc6a6ee4b0c2f1c0eb7a93071
OttoOikarinen <oikarinenotto@gmail.com>
2023-01-11 13:46:06 +0200

    Modified DatePicker for a range

--- src/components/AskDate/AskDate.jsx
+++ src/components/AskDate/AskDate.jsx
@@ -5,9 +5,26 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 
 const AskDate = () => {
-    const [startDate, setStartDate] = React.useState(new Date());
+    const [startDate, setStartDate] = React.useState(new Date("2022/12/10"));
+    const [endDate, setEndDate] = React.useState(new Date("2023/01/10"));
     return (
-      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      <>
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+          minDate={startDate}
+        />
+      </>
     );
   };
 
