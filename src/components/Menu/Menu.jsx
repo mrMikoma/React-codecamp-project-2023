9c0e0ab73cb5f29446676d0f98d7a7d2c1cfc20e
OttoOikarinen <oikarinenotto@gmail.com>
2023-01-15 20:30:29 +0200

    Finished menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -11,21 +11,40 @@ const Menu = () => {
 
     return (
         <div className="menu">
-            <h3>Menu</h3>
+            <div className="header">
+                <h3>MENU</h3>
+            </div>
 
-            <BoltIcon />
-            <Link to=""> Front Page</Link>
+            <div className="button">
 
+                <Button variant="contained"
+                    component={Link}
+                    startIcon={<BoltIcon />}
+                    to="">
+                    Front Page
+                </Button>
 
-            <Button variant="outlined"
-                startIcon={<CalculateIcon
-                to="calculator"/>}>
-                Calculator
-            </Button>
-            <CalculateIcon />
-            <Link to="calculator"> Calculator</Link>
-            <EuroSymbolIcon />
-            <Link to="previous-page"> Previous Prices</Link>
+            </div>
+            <div className="button">
+
+                <Button variant="contained"
+                    component={Link}
+                    startIcon={<CalculateIcon />}
+                    to="calculator">
+                    Calculator
+                </Button>
+
+            </div>
+            <div className="button">
+                <Button variant="contained"
+                    component={Link}
+                    startIcon={<EuroSymbolIcon />}
+                    to="previous-page">
+                    Previous Prices
+                </Button>
+
+            </div>
+            
         </div>
     )
 }
