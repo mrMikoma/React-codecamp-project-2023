9c2b42b7652debbe4827b7fbd32cd08a766cf3b7
OttoOikarinen <oikarinenotto@gmail.com>
2023-01-17 08:14:00 +0200

    Made changes to ui

--- src/components/AskDate/AskDate.jsx
+++ src/components/AskDate/AskDate.jsx
@@ -8,23 +8,25 @@ const AskDate = () => {
     const [startDate, setStartDate] = React.useState(new Date("2022/12/10"));
     const [endDate, setEndDate] = React.useState(new Date("2023/01/10"));
     return (
-      <>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-        />
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-        />
-      </>
+      <div className='date-picker'>
+        <>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+          />
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+          />
+        </>
+      </div>
     );
   };
 
